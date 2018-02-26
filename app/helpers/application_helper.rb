5748bf4cb6f505a7320672feae087244eba9849a
Jack Reuter <djreuter45@gmail.com>
2018-02-26 15:52:54 -0700

    added some helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  helper_method :current_user, :logged_in?, :authorize
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def authorize
+    redirect_to root_path unless current_user
+  end
 end
