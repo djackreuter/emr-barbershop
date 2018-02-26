ef3f066b24a2510aa93b406f74c9c57d3f8bc3a6
Jack Reuter <djreuter45@gmail.com>
2018-02-26 13:24:45 -0700

    started user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to root_path
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
+  end
+
 end
