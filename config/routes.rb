1b77ae38a67d99a2204d64873f199334577b74bb
Jack Reuter <djreuter45@gmail.com>
2018-02-26 15:33:04 -0700

    updated routes and added users show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'barbershops#index'
   resources :barbershops
   resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
