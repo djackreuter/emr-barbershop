018d4188c4ece4c7a6971daae4e31acec59f30af
Jack Reuter <djreuter45@gmail.com>
2018-02-27 15:19:33 -0700

    added some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 60 }
+  validates :password_digest, presence: true, length: { minimum: 6 }, on: :create
 end
