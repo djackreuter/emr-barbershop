dc915f7920ee6d7010ae425c6fb34c4866c9dd5c
Jack Reuter <djreuter45@gmail.com>
2018-02-28 13:34:12 -0700

    changed create method to accomidate confirmation email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params.dig(:email))
     if user && user.authenticate(params.dig(:password))
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-      flash[:alert] = 'Login successful!'
+      if user.email_confirmed
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+        flash[:alert] = 'Login successful!'
+      else
+        flash[:alert] = 'Please confirm your account first'
+      end
     else
       redirect_to root_path
       flash[:alert] = 'Email or password is incorrect'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-    else
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver_later
+      flash[:alert] = "An email was sent to #{@user.email}. Click the link to confirm your account"
       redirect_to root_path
+      # session[:user_id] = @user.id
+      # redirect_to user_path(@user)
+    else
+      flash[:alert] = 'Something went wrong, could not register user'
     end
   end
+
+  def confirm_email
+    user = User.find_by_confirm_token(params.dig(:id))
+    if user
+      user.email_activate
+      flash[:alert] = 'Your email has been confirmed, please sign in to continue'
+      redirect_to root_path
+    else
+      flash[:alert] = 'User does not exist'
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
