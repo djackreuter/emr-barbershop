26831a74cab1898f042d67489bb47b3e307cbb7a
Jack Reuter <djreuter45@gmail.com>
2018-02-28 13:34:34 -0700

    changed method name to match view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: 'admin@emr-barbershop.com'
-  def send_confirmation_email(user)
+  def registration_confirmation(user)
     @user = user
     mail to: @user.email, subject: 'Please Confirm Your Account'
   end
