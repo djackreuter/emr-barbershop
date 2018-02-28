39aa4c4510dbe9da6e6e50015cc0272516be23dd
Jack Reuter <djreuter45@gmail.com>
2018-02-28 10:11:39 -0700

    added method to create confirmation token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  before_create :confirmation_token
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, length: { maximum: 60 }
   validates :password_digest, presence: true, length: { minimum: 6 }, on: :create
+
+  private
+
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
+  end
 end
