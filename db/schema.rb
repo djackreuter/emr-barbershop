471e8920e1ac6d9887e91cc1169797a6bbfbe735
Jack Reuter <djreuter45@gmail.com>
2018-03-01 14:51:19 -0700

    added user_id (fk) to barbershops

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228205807) do
+ActiveRecord::Schema.define(version: 20180301214926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
