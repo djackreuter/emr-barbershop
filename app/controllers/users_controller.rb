86863a76dab0f886be68975713a024fe99e90605
Jack Reuter <djreuter45@gmail.com>
2018-03-05 10:22:53 -0700

    changed barbershops for new association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @barbershop = Barbershop.find(params[:barbershop_id])
+  end
+
   def new
     @user = User.new
-    @user.barbershops.build
+    @user.build_barbershop
   end
 
   def create
