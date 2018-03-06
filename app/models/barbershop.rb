3ba4a6a1b8c4ff0bfbf9227e8098daa54934dad1
Jack Reuter <djreuter45@gmail.com>
2018-03-06 11:03:28 -0700

    added validation for price

--- app/models/barbershop.rb
+++ app/models/barbershop.rb
@@ -3,4 +3,5 @@ class Barbershop < ApplicationRecord
   has_many :users, through: :members
   validates :barbershop_name, presence: true
   validates :bio, length: { maximum: 500 }
+  validates :price, presence: true
 end
