22b51b378ead0b26733821b81429f30b16ee9fe9
Jack Reuter <djreuter45@gmail.com>
2018-03-06 11:03:11 -0700

    added show, new, and create methods to barbershop controller

--- app/controllers/barbershops_controller.rb
+++ app/controllers/barbershops_controller.rb
@@ -2,4 +2,29 @@ class BarbershopsController < ApplicationController
   def index
     @barbershops = Barbershop.all
   end
+
+  def show
+    @barbershop = Barbershop.find(params[:id])
+  end
+
+  def new
+    @barbershop = Barbershop.new
+  end
+
+  def create
+    @barbershop = Barbershop.new(barbershop_params)
+    if @barbershop.save
+      flash[:alert] = 'Barbershop created successfuly'
+      redirect_to barbershop_path(@barbershop)
+    else
+      flash[:alert] = 'Something went wrong please try again'
+      redirect_to edit_user_path(current_user)
+    end
+  end
+
+  private
+
+  def barbershop_params
+    params.require(:barbershop).permit(:barbershop_name, :bio, :price)
+  end
 end
