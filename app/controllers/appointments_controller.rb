0a4bd2e3df77e317525c808fe9588553a3d7e278
Jack Reuter <djreuter45@gmail.com>
2018-03-07 11:49:46 -0700

    added new and create methods

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,11 @@
 class AppointmentsController < ApplicationController
+  def new
+    @appointment = Appointment.new
+    @barbershop = Barbershop.find(params[:barbershop_id])
+  end
+
+  def create
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @appointment = @barbershop.appointments.build
+  end
 end
