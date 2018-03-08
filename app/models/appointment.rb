e3043f9b83083c3952921484ad56bd35a2686442
Jack Reuter <djreuter45@gmail.com>
2018-03-08 15:58:43 -0700

    added validations for appointment and post

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :barbershop, inverse_of: :appointments
+  validates :cust_name, presence: true
+  validates :appt_time, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :postable, polymorphic: true
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { maximum: 1000 }
 end
