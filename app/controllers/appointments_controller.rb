ece17a7bf950f47db91193e814ab51ad799cc275
Jack Reuter <djreuter45@gmail.com>
2018-03-08 11:35:47 -0700

    added create and destroy methods

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,11 +1,33 @@
 class AppointmentsController < ApplicationController
+  # respond_to :js
   def new
-    @appointment = Appointment.new
+    @appointment = Appointment.new(appt_time: Time.zone.now)
     @barbershop = Barbershop.find(params[:barbershop_id])
   end
 
   def create
     @barbershop = Barbershop.find(params[:barbershop_id])
-    @appointment = @barbershop.appointments.build
+    @appointment = @barbershop.appointments.create(appt_params)
+    respond_to do |format|
+      if @appointment.save
+        format.js
+      else
+        flash[:alert] = 'Something went wrong, please try again'
+        render template: 'appointments/new'
+      end
+    end
+  end
+
+  def destroy
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @appointment = @barbershop.appointments.find(params[:id])
+    @appointment.destroy
+    redirect_to barbershop_path(@barbershop)
+  end
+
+  private
+
+  def appt_params
+    params.require(:appointment).permit(:cust_name, :appt_time)
   end
 end
