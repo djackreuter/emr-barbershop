e2d97ef86562124b7f566031b9b4af04f65afaaa
Jack Reuter <djreuter45@gmail.com>
2018-03-08 14:06:47 -0700

    updated routes and added new.js view for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'barbershops#index'
   resources :barbershops do
+    resources :posts
     resources :users
     resources :appointments, only: [:create, :index, :new, :destroy]
   end
   resources :users do
+    resources :posts
     member do
       get :confirm_email
     end
