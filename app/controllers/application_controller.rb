7e5bec6a9de2f19eba79ec9dd3db51aa2da0c7fd
Jack Reuter <djreuter45@gmail.com>
2018-03-08 11:35:14 -0700

    added method to set timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_time_zone
   protect_from_forgery with: :exception
   include ApplicationHelper
+  def set_time_zone
+    Time.zone = 'America/Denver'
+  end
 end
