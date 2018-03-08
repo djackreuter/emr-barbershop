15f65d649e059afd475756be22a04c3b003b5008
Jack Reuter <djreuter45@gmail.com>
2018-03-08 14:56:46 -0700

    added new, create, and destroy methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  def new
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @post = Post.new
+  end
+
+  def create
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @post = @barbershop.posts.create(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js
+      else
+        flash[:alert] = 'Something bad happened and your post wasn\'t published. Try again'
+        render template 'posts/new'
+      end
+    end
+  end
+
+  def destroy
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @post = @barbershop.posts.find(params[:id])
+    @post.destroy
+    redirect_to barbershop_path(@barbershop)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
