d7d8bfcf5ed3debe476c35c40362f5d1b3a7fc5b
Jack Reuter <djreuter45@gmail.com>
2018-03-09 12:35:58 -0700

    added private to params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :private)
   end
 
   def find_postable
