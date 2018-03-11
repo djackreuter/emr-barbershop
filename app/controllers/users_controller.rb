49706050a3e6518830ab84aff321acfee86ef139
Jack Reuter <djreuter45@gmail.com>
2018-03-11 11:04:46 -0600

    minor updates and corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize, only: [:edit, :create, :update, :destroy]
+  before_action :authorize, only: [:edit, :update, :destroy]
   def index
     @users = User.all
   end
