c454c5eae3bd007a5356e33cd9a2ee58cd45bb9b
Jack Reuter <djreuter45@gmail.com>
2018-03-12 12:45:07 -0600

    added posts tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:postable) { Barbershop.new }
+  let(:postable) { User.new }
+  before do
+    postable.save(validate: false)
+  end
+
+  context 'validation tests' do
+    it 'ensures post title present' do
+      post = Post.new(postable: postable, body: Faker::Hipster.paragraph(2)).save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures post body present' do
+      post = Post.new(postable: postable, title: 'Test Post Title').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures user post is saved successfully' do
+      post = Post.new(postable: postable, title: 'Test Post Title', body: Faker::Hipster.paragraph(2)).save
+      expect(post).to eq(true)
+    end
+
+    it 'ensures barbershop post is saved successfully' do
+      post = Post.new(postable: postable, title: 'Test Post Title', body: Faker::Hipster.paragraph(2)).save
+      expect(post).to eq(true)
+    end
+  end
 end
