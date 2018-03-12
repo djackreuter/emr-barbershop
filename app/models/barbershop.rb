abdb0bcf4b125c6db674c4460f917d00a4111122
Jack Reuter <djreuter45@gmail.com>
2018-03-12 12:00:46 -0600

    added barbershop test and additional validations

--- app/models/barbershop.rb
+++ app/models/barbershop.rb
@@ -3,9 +3,9 @@ class Barbershop < ApplicationRecord
   has_many :members
   has_many :posts, as: :postable, dependent: :destroy
   has_many :users, through: :members
-  validates :barbershop_name, presence: true
+  validates :barbershop_name, presence: true, uniqueness: true
   validates :bio, length: { maximum: 500 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 
   def view_name
     barbershop_name
--- spec/models/barbershop_spec.rb
+++ spec/models/barbershop_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Barbershop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures barbershop name presence' do
+      barbershop = Barbershop.new(bio: Faker::HowIMetYourMother.quote, price: 10).save
+      expect(barbershop).to eq(false)
+    end
+
+    it 'ensures bio length is < 500' do
+      barbershop = Barbershop.new(barbershop_name: 'The Happy Mullet', bio: Faker::Lorem.characters(501), price: 20).save
+      expect(barbershop).to eq(false)
+    end
+
+    it 'ensures price is present' do
+      barbershop = Barbershop.new(barbershop_name: 'The Happy Mullet', bio: Faker::HowIMetYourMother.quote).save
+      expect(barbershop).to eq(false)
+    end
+
+    it 'ensures barbershop is saved' do
+      barbershop = Barbershop.new(barbershop_name: 'The Happy Mullet', bio: Faker::HowIMetYourMother.quote, price: 10).save
+      expect(barbershop).to eq(true)
+    end
+
+    it 'ensures barbershop name is unique' do
+      barbershop = Barbershop.new(barbershop_name: 'The Happy Mullet', bio: Faker::HowIMetYourMother.quote, price: 10).save
+      barbershop2 = Barbershop.new(barbershop_name: 'The Happy Mullet', bio: Faker::HowIMetYourMother.quote, price: 10).save
+      expect(barbershop2).to eq(false)
+    end
+  end
 end
