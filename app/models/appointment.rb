07d3de6590c45c1043b188c8cd2f5ba26848c097
Jack Reuter <djreuter45@gmail.com>
2018-03-12 10:00:56 -0600

    ordered appointments by appt_time instead of created at

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :barbershop, inverse_of: :appointments
   validates :cust_name, presence: true
   validates :appt_time, presence: true
-  scope :ordered, -> { order('created_at DESC') }
+  scope :order_by_appt, -> { order('appt_time') }
 end
