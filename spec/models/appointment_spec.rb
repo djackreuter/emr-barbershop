15e21e2091ba98650bd8d09dbb740498051feddc
Jack Reuter <djreuter45@gmail.com>
2018-03-12 12:19:42 -0600

    added appointment test

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:barbershop){ Barbershop.new }
+  before do
+    barbershop.save(validate: false)
+  end
+  context 'validation tests' do
+    it 'ensures customer name is present' do
+      appt = Appointment.new(appt_time: Time.zone.now).save
+      expect(appt).to eq(false)
+    end
+
+    it 'ensures appointment time is present' do
+      appt = Appointment.new(cust_name: 'Jimmy Hopkins').save
+      expect(appt).to eq(false)
+    end
+
+    it 'ensures appointment is created successfully' do
+      appt = Appointment.new(barbershop: barbershop, cust_name: 'Jimmy Hopkins', appt_time: Time.zone.now).save
+      expect(appt).to eq(true)
+    end
+  end
 end
